6b23146212465decff5b9853550518f71b9bb972
dhyghytg <msjcishukachula@hotmail.com>
2024-05-13 03:23:37 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const randomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+function wordBreak(s, wordDict) {
+  const wordSet = new Set(wordDict);
+  const dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let end = 1; end <= s.length; end++) {
+    for (let start = 0; start < end; start++) {
+      if (dp[start] && wordSet.has(s.substring(start, end))) {
+        dp[end] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+}
